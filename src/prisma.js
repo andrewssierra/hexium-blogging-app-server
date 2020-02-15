739d4835a8b4e0daadf667618a1156d3f6a0f7ff
andrewssierra <andrewssierra@yahoo.com>
2020-02-15 12:50:50 -0800

    Extract prisma secret to env var

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from './resolvers';
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: 'c905e439-38a6-40bc-b667-4ac07b95dacf',
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements
 });
 
