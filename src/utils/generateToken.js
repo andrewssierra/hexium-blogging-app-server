64a9160f23ce5695a4bcc05e2f8f32a6b5b9a694
andrewssierra <andrewssierra@yahoo.com>
2020-02-15 13:00:24 -0800

    move jwt secret to env var

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const getWebToken = userId => {
-    return jwt.sign({ userId }, '10', { expiresIn: '12 h' });
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '12 h' });
 };
 
 export { getWebToken as default };
