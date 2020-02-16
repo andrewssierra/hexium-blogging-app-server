e2a9c629bc4a95dd8a8e15e6c35da88003b7b66b
andrewssierra <andrewssierra@yahoo.com>
2020-02-16 15:28:51 -0800

    Adding unit tests.

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,9 +1,10 @@
 import 'cross-fetch/polyfill';
 import { gql } from 'apollo-boost';
-import seedDatabase, {userOne} from './utils/seedDatabase';
+import seedDatabase, { userOne, postOne, postTwo } from './utils/seedDatabase';
 import getClient from './utils/getClient';
+import prisma from '../src/prisma';
 
-const client = getClient()
+const client = getClient();
 
 beforeEach(seedDatabase);
 test('should only return published posts', async () => {
@@ -23,9 +24,9 @@ test('should only return published posts', async () => {
 });
 
 test('should fetch my posts', async () => {
-    const client = getClient(userOne.jwt)
+    const client = getClient(userOne.jwt);
     const getMyPosts = gql`
-        query{
+        query {
             myPosts {
                 title
                 body
@@ -33,7 +34,69 @@ test('should fetch my posts', async () => {
                 published
             }
         }
-    `
-    const {data} = await client.query({query: getMyPosts})
+    `;
+    const { data } = await client.query({ query: getMyPosts });
     expect(data.myPosts.length).toBe(2);
-})
+});
+
+test('should be able to update own post', async () => {
+    const client = getClient(userOne.jwt);
+    const updatePost = gql`
+        mutation {
+            updatePost (
+                id: "${postOne.post.id}",
+                data: {
+                    published: false
+                }
+            ){
+                id
+                title
+                body
+                published
+            }
+        }
+    `;
+    const { data } = await client.mutate({ mutation: updatePost });
+    const exists = await prisma.exists.Post({
+        id: postOne.post.id,
+        published: false
+    });
+    expect(data.updatePost.published).toBe(false);
+    expect(exists).toBe(true);
+});
+
+test('should create a new post', async () => {
+    const client = getClient(userOne.jwt);
+    const createPost = gql`
+        mutation {
+            createPost(
+                data: { title: "test post", body: "test post", published: true }
+            ) {
+                title
+                body
+                published
+            }
+        }
+    `;
+    const { data } = await client.mutate({ mutation: createPost });
+    const exists = await prisma.exists.Post({ title: 'test post' });
+    expect(data.createPost.title).toBe('test post');
+    expect(data.createPost.published).toBe(true);
+    expect(data.createPost.body).toBe('test post');
+    expect(exists).toBe(true);
+});
+
+test('should delete post', async () => {
+    const client = getClient(userOne.jwt);
+
+    const deletePost = gql`
+        mutation {
+            deletePost(id: "${postTwo.post.id}"){
+                id
+            }
+        }
+    `;
+    await client.mutate({ mutation: deletePost });
+    const exists = await prisma.exists.Post({ id: postTwo.post.id });
+    expect(exists).toBe(false);
+});
--- tests/utils/seedDatabase.js
+++ tests/utils/seedDatabase.js
@@ -1,6 +1,6 @@
-import bcrypt from 'bcryptjs'
-import prisma from '../../src/prisma'
-import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs';
+import prisma from '../../src/prisma';
+import jwt from 'jsonwebtoken';
 
 const userOne = {
     input: {
@@ -12,35 +12,61 @@ const userOne = {
     jwt: undefined
 };
 
+const postOne = {
+    input: {
+        title:
+            'Ten Beautiful Reasons We Can not Help But Fall In Love With Food.',
+        body: 'food food food food',
+        published: true
+    },
+    post: undefined
+};
+
+const postTwo = {
+    input: {
+        title: 'Understanding The Background Of Food.',
+        body: 'food food food food',
+        published: false
+    },
+    post: undefined
+};
+
 const seedDatabase = async () => {
     await prisma.mutation.deleteManyPosts();
     await prisma.mutation.deleteManyUsers();
 
     userOne.user = await prisma.mutation.createUser({
         data: {
-            ... userOne.input,
-            posts: {
-                create: [
-                    {
-                        title:
-                            'Ten Beautiful Reasons We Can not Help But Fall In Love With Food.',
-                        body: 'food food food food',
-                        published: true
-                    },
-                    {
-                        title: 'Understanding The Background Of Food.',
-                        body: 'food food food food',
-                        published: false
-                    }
-                ]
+            ...userOne.input
+        }
+    });
+
+    postOne.post = await prisma.mutation.createPost({
+        data: {
+            ...postOne.input,
+            author: {
+                connect: {
+                    id: userOne.user.id
+                }
             }
         }
     });
-    
-     userOne.jwt = await jwt.sign(
-         { userId: userOne.user.id },
-         process.env.JWT_SECRET
-     );
+
+    postTwo.post = await prisma.mutation.createPost({
+        data: {
+            ...postTwo.input,
+            author: {
+                connect: {
+                    id: userOne.user.id
+                }
+            }
+        }
+    });
+
+    userOne.jwt = await jwt.sign(
+        { userId: userOne.user.id },
+        process.env.JWT_SECRET
+    );
 };
 
-export {seedDatabase as default, userOne}
+export { seedDatabase as default, userOne, postOne, postTwo };
